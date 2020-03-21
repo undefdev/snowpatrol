2fca9fdfec2ad11efffcc11673978fcbd77ce506
undef <invalid@example.com>
2020-03-21 14:48:38 +0100

    can delete from heatmaps now

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-W = 464
-H = 640
 function read_tsv_image( filename, width, height )
 	local score_text = love.filesystem.newFile( filename )
 	local imageData = love.image.newImageData( width, height, "rgba32f" )
@@ -32,8 +30,9 @@ function write_tsv_image( filename, imageData )
 	assert( file:write( table.concat( fileTable, '\n', 0 ) ) )
 end
 
-function remove_box_from_imageData( imageData, x, y, w, h )
-	return imageData:mapPixel( function()  return 0, 0, 0, 0.5  end, x, y, w, h )
+function remove_box_from_heatmap( hm, x, y, w, h )
+	hm.data:mapPixel( function()  return 0, 0, 0, 0.5  end, x, y, w, h )
+	return hm.img:replacePixels( hm.data )
 end
 function apply_threshold_to_imageData( imageData, threshold )
 	return imageData:mapPixel( function( x, y, r, g, b, a )
@@ -48,6 +47,9 @@ function love.load()
 	love.mouse.setVisible( false )
 	t = 0
 	w, h = love.graphics.getDimensions()
+	W = 464
+	H = 640
+	RATIO = h/H
 	img = love.graphics.newImage"test.jpg"
 	heatmap_score = read_tsv_image( "test.tsv", W, H )
 	--write_tsv_image( "test_out.tsv", heatmap_score.data )
@@ -69,15 +71,14 @@ end
 
 function love.draw()
 	local he = img:getHeight()
-	local ratio = h/H
 	if isShowingImages then
-		love.graphics.draw( img, 0, 0, 0, H/he*ratio, H/he*ratio )
-		love.graphics.draw( img, W*ratio, 0, 0, H/he*ratio, H/he*ratio )
+		love.graphics.draw( img, 0, 0, 0, H/he*RATIO, H/he*RATIO )
+		love.graphics.draw( img, W*RATIO, 0, 0, H/he*RATIO, H/he*RATIO )
 	end
 	if isShowingHeatmaps then
 		love.graphics.setShader( shader )
-		love.graphics.draw( heatmap_score.img, 0, 0, 0, ratio, ratio )
-		love.graphics.draw( heatmap_link.img, ratio*W, 0, 0, ratio, ratio )
+		love.graphics.draw( heatmap_score.img, 0, 0, 0, RATIO, RATIO )
+		love.graphics.draw( heatmap_link.img, RATIO*W, 0, 0, RATIO, RATIO )
 		love.graphics.setShader()
 	end
 	local mx, my = love.mouse.getPosition()
@@ -85,18 +86,19 @@ function love.draw()
 	love.graphics.setColor( 1, 0.5, 0 )
 	love.graphics.circle( "fill", mx, my, 2 )
 	if removeMode>0 then
-		local boxOffsetX, boxOffsetY
+		local boxWidth, boxHeight
 		love.graphics.print( mode[removeMode], mx, my )
 		if boxX then
-  			boxOffsetX, boxOffsetY = mx - boxX, my - boxY
-			love.graphics.rectangle( "line", boxX, boxY, boxOffsetX, boxOffsetY )
+  			boxWidth, boxHeight = mx - boxX, my - boxY
+			love.graphics.rectangle( "line", boxX, boxY, boxWidth, boxHeight )
 		end
-		if removeMode==1 then
-			local offset = ratio*W
+		if removeMode==1 or removeMode==3 then
+			local offset = RATIO*W
 			mx_ = mx<=offset and mx + offset or mx - offset
 			love.graphics.circle( "fill", mx_, my, 2 )
+			love.graphics.print( mode[removeMode], mx_, my )
 			if boxX then
-				love.graphics.rectangle( "line", boxX + offset, boxY, boxOffsetX, boxOffsetY )
+				love.graphics.rectangle( "line", boxX + offset, boxY, boxWidth, boxHeight )
 			end
 		end
 	end
@@ -104,7 +106,7 @@ function love.draw()
 end
 keys = {
 	escape = love.event.quit,
-	r = function()  removeMode = (removeMode + 1)%4;  if removeMode==0 and boxX then  boxX, boxY = nil  end  end,
+	r = function()  removeMode = (removeMode + 1)%4;  if removeMode==0 then  deleteBox()  end  end,
 	space = function()  isShowingImages = not isShowingImages  end,
 	m = function()  isShowingHeatmaps = not isShowingHeatmaps  end,
 	tab = function()
@@ -113,12 +115,42 @@ keys = {
 	end,
 }
 
+function deleteBox()  boxX, boxY = nil  end
+function descaleCoordinates( a, ... )
+	if a then  return a/RATIO, descaleCoordinates( ... )  end
+end
+function clamp( x, a, b )  return math.min( math.max( x, a ), b )  end
+function processBoxCoordinates( x, y, w, h )
+	local x, y, w, h = descaleCoordinates( x, y, w, h )
+	if w<0 then  x = x + w;  w = -w  end
+	if h<0 then  y = y + h;  h = -h  end
+	x, y = clamp( x, 0, W     ), clamp( y, 0, H     )
+	w, h = clamp( w, 0, W - x ), clamp( h, 0, H - y )
+	return x, y, w, h
+end
+function getHeatmap( x, y )
+	if x<=W then  return heatmap_score  end
+	return heatmap_link
+end
+function removeBox( mode, x, y, w, h )
+	local x, y, w, h = processBoxCoordinates( x, y, w, h )
+	if w==0 or h==0 then  return deleteBox() end
+	if mode<=2 then
+		hm = getHeatmap( x, y )
+		remove_box_from_heatmap( hm, x, y, w, h )
+	end
+	if mode%2==1 then
+		hm = getHeatmap( x + W, y )
+		remove_box_from_heatmap( hm, x, y, w, h )
+	end
+	deleteBox()
+end
 function love.mousepressed( x, y, l, r )
 	if removeMode>0 and not boxX then
 		boxX, boxY = x, y
-	else
-		boxX, boxY = nil
+		return
 	end
+	return removeBox( removeMode, boxX, boxY, x - boxX, y - boxY )
 end
 
 function love.keypressed( k )
